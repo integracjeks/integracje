31304a590b4f5de750f8661dfe3d0489ba8175dc
kacper.sledz <casper.sledx@gmail.com>
2016-10-25 01:10:07 +0200

    book cleanup

--- Integracje/Integracje/Model/Book.cs
+++ Integracje/Integracje/Model/Book.cs
@@ -23,50 +23,5 @@ namespace Integracje.Model
         public string authors_gender { get; set; }
         public string original_lanuguage { get; set; }
         public int translated_languages_quantity { get; set; }
-
-
-
-        string createQuery = "CREATE TABLE[dbo].[Books]("+
-            " [id] [int] NOT NULL, "+
-            " [title] [varchar](250) NOT NULL,"+
-            " [pages] [int] not null, "+
-            " [year] [int] not null, "+
-            " [isbn] [varchar](250) NOT NULL ," +
-            " [genre] [varchar](250) NOT NULL ," +
-            " [price] [varchar](250) NOT NULL ," +
-            " [authors_first_name] [varchar](250) NOT NULL ," +
-            " [authors_last_name] [varchar](250) NOT NULL ," +
-            " [fact_based] [int] NOT NULL ," +
-            " [toms_quantity] [int] NOT NULL ," +
-            " [authors_email] [varchar](250) NOT NULL ," +
-            " [authors_gender] [varchar](250) NOT NULL ," +
-            " [original_lanuguage] [varchar](250) NOT NULL ," +
-            " [translated_languages_quantity] [int] NOT NULL " +
-            ")";
-
-/*
- *      CREATE TABLE [dbo].[Tools]( [id][int] NOT NULL, [typ] [int] NOT NULL, [name] [varchar](255) NULL, [manufacturer] [varchar](255) NULL, [model] [varchar](255) NULL, [barcode] [varchar](255) NULL, [size] [int] NULL, [lower_band] [int] NULL, [upper_band] [int] NULL, [spec_size] [varchar](255) NULL, [spec_type] [varchar](255) NULL, [voltage_in_v] [int] NULL, [capacity] [int] NULL, [power_in_wat] [int] NULL, [update_date] [datetime] NULL )
- * 
- * 
-        CREATE TABLE[dbo].[Tools]( [id]
-        [int] NOT NULL, [typ] [int] NOT NULL, [name] [varchar](255) NULL, [manufacturer]
-        [varchar](255) NULL,
-	                                                    [model]
-        [varchar](255) NULL, [barcode]
-        [varchar](255) NULL, [size]
-        [int] NULL, [lower_band]
-        [int] NULL, [upper_band]
-        [int] NULL,
-                                                        [spec_size]
-        [varchar](255) NULL, [spec_type]
-        [varchar](255) NULL, [voltage_in_v]
-        [int] NULL, [capacity]
-        [int] NULL,
-                                                        [power_in_wat]
-        [int] NULL, [update_date]
-        [datetime]
-        NULL )
-        
-         */
     }
 }
